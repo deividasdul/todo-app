043dba2e51cdcf762d533984ea160cb5fe2f3b4d
deividasdul <deividas.dul@gmail.com>
2024-03-15 14:38:53 +0200

    Second commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const APP = express();
 
 APP.use(express.static("public"));
 APP.use(express.urlencoded({ extended: true }));
+APP.use(express.json());
 
 APP.listen(PORT, () => {
   console.log("Starting...");
@@ -14,4 +15,10 @@ APP.get("/", (req, res) => {
   res.render("index.ejs", {
     year: new Date().getFullYear(),
   });
+});
+
+APP.post("/add", (req, res) => {
+  var taskName = req.body.tName;
+  var taskDate = req.body.tDate;
+  var taskDescription = req.body.tDesc;
 });
